afb7d262b364a16e1fb95da27195a836791d4b36
xianhai <xianhaic@gmail.com>
2019-09-29 00:19:16 -0700

    Create and fetch sketches now working

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const compression = require('compression')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const app = express()
+const uuidv4 = require('uuid/v4')
 const router = express.Router()
 const AWS = require('aws-sdk')
 const dynamodb = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'})
@@ -29,74 +31,169 @@ router.use(awsServerlessExpressMiddleware.eventContext())
 // NOTE: tests can't find the views directory without this
 app.set('views', path.join(__dirname, 'views'))
 
+const fetch = require("node-fetch")
+const serverTokenDurationSec = 30
+const ext = {
+    secret: process.env.EXT_SECRET,
+    clientId: process.env.EXT_CLIENT_ID,
+    ownerId: process.env.EXT_OWNER_ID
+}
+
+// Verify the header and the enclosed JWT.
+function verifyAndDecode (header) {
+  if (header.startsWith(bearerPrefix)) {
+    try {
+      const token = header.substring(bearerPrefix.length);
+      return jsonwebtoken.verify(token, secret, { algorithms: ['HS256'] });
+    }
+    catch (ex) {
+      throw Boom.unauthorized(STRINGS.invalidJwt);
+    }
+  }
+  throw Boom.unauthorized(STRINGS.invalidAuthHeader);
+}
+
+// params: channel_id
 router.get('/sketches', async (req, res) => {
+  //let payload = verifyAndDecode(req.headers.authorization)
   let sketches = await getSketchesByChannel(req.query.channel_id)
   res.json(sketches)
 })
 
-const getSketchesByChannel = async (channelId) => {
+// params: channel_id
+router.post('/sketches', async (req, res) => {
+  //let payload = verifyAndDecode(req.headers.authorization)
+  const { user_id, display_name, sketch } = req.body
+  const { channel_id } = req.query
+
+  console.log(user_id);
+  console.log(display_name);
+  console.log(sketch);
+
+  let newSketch = await createSketchInChannel(
+    channel_id, user_id, display_name, sketch)
+
+  // pubsub broadcast
+    /*broadcastToAll({
+    event: 'create-sketch',
+    sketch: newSketch
+  }, makeServerToken(channelId), channelId)
+  */
+
+  res.json(newSketch);
+})
+
+// params: sketch_id
+router.delete('/sketches', async (req, res) => {
+  let payload = verifyAndDecode(req.headers.authorization)
+    /*const { role } = payload;
+
+  if (role !== "broadcaster" || role !== "moderator") {
+    res.json({ errors: ["Unauthorized action"] })
+  }*/
+
+  await deleteSketch(req.query.sketch_id)
+  broadcastToAll({
+    event: 'delete-sketch',
+    sketch_id: req.query.sketch_id
+  }, makeServerToken(channelId), channelId)
+
+  res.json({status: 'deleted'})
+})
+
+function makeServerToken(channelId) {
+  const payload = {
+    exp: Math.floor(Date.now() / 1000) + serverTokenDurationSec,
+    channel_id: channelId,
+    user_id: ext.ownerId, // extension owner ID for the call to Twitch PubSub
+    role: 'external',
+    pubsub_perms: {
+      send: [ '*' ],
+    },
+  }
+
+  //const secret = Buffer.from(ext.secret, 'base64');
+  const secret = Buffer.from("REPLACE LATER", 'base64');
+  return jwt.sign(payload, secret, { algorithm: 'HS256' });
+}
+
+const createSketchInChannel = async (channelId, userId, displayName, sketch) => {
+  const id = uuidv4();
   var params = {
     TableName: sketchesTable,
+    Item: {
+      sketch_id: id, channel_id: channelId, user_id: userId, display_name: displayName, sketch
+    },
+  }
+
+  try{
+    let data = await dynamodb.put(params).promise()
+    console.log(`Created sketch for channel_id: ${channelId} by user_id: ${userId} (${displayName})`)
+    return params.Item;
+  } catch(error) {
+    return error;
+  }
+}
+
+const deleteSketchFromChannel = async (sketchId) => {
+  var params = {
     Key: {
-      "sketch_id": channelId
-    }
- };
-  console.log("NUM");
-  console.log(params.Key.sketch_id);
-  console.log(channelId);
+      ":sketchId": sketchId
+    },
+    TableName: sketchesTable
+  };
   try {
-    let data = await dynamodb.get(params).promise();
-    console.log(data);
-    return data;
+    let data = dynamodb.deleteItem(params).promise()
+    console.log(`Deleted sketch from channel sketch_id: ${sketchId}`)
+  }
+  catch(err) {
+    console.log(err);
+  }
+
+}
+
+const getSketchesByChannel = async (channelId) => {
+  var params = {
+    ExpressionAttributeValues: {
+      ":channelId": channelId
+    },
+    KeyConditionExpression: "channel_id = :channelId",
+    TableName: sketchesTable,
+    ProjectionExpression: "sketch",
+    IndexName: "Channel"
+  };
+
+  try {
+    let data = await dynamodb.query(params).promise();
+    console.log(`Fetched sketches for channel_id: ${channelId}`)
+    return data.Items;
   }
   catch(err) {
     console.log(err);
   }
 }
 
-router.get('/sam', (req, res) => {
-  res.sendFile(`${__dirname}/sam-logo.png`)
-})
+const broadcastToAll = async (data, token, channelId) => {
+  // use twitch pubsub
 
-router.post('/users', (req, res) => {
-  const user = {
-    id: ++userIdCounter,
-    name: req.body.name
-  }
-  users.push(user)
-  res.status(201).json(user)
-})
-
-router.put('/users/:userId', (req, res) => {
-  const user = getUser(req.params.userId)
-
-  if (!user) return res.status(404).json({})
-
-  user.name = req.body.name
-  res.json(user)
-})
-
-router.delete('/users/:userId', (req, res) => {
-  const userIndex = getUserIndex(req.params.userId)
-
-  if (userIndex === -1) return res.status(404).json({})
-
-  users.splice(userIndex, 1)
-  res.json(users)
-})
-
-const getUser = (userId) => users.find(u => u.id === parseInt(userId))
-const getUserIndex = (userId) => users.findIndex(u => u.id === parseInt(userId))
-
-// Ephemeral in-memory data store
-const users = [{
-  id: 1,
-  name: 'Joe'
-}, {
-  id: 2,
-  name: 'Jane'
-}]
-let userIdCounter = users.length
+  fetch(`https://api.twitch.tv/extensions/message/${channelId}`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      content_type: 'application/json',
+      message: JSON.stringify(data),
+      targets: ['broadcast']
+    })
+  })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => { console.log(err) });
+}
 
 // The aws-serverless-express library creates a server and listens on a Unix
 // Domain Socket for you, so you can remove the usual call to app.listen.
